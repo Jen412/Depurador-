287f9d53dcb04c0d9edeec328d73e8f027fbd469
Jen412 <72884006+Jen412@users.noreply.github.com>
2021-03-23 12:10:51 -0600

    cambios

--- TablaTokens.java
+++ TablaTokens.java
@@ -44,10 +44,10 @@ public class TablaTokens {
         compAlg[1] = "min";
         compAlg[2] = "mayi";
         compAlg[3] = "mini";
-        compAlg[4] = "yy";
-        compAlg[5] = "oo";
-        compAlg[6] = "===";
-        compAlg[7] = "|=";
+        compAlg[4] = "===";
+        compAlg[5] = "|=";
+        compAlg[6] = "yy";
+        compAlg[7] = "oo";
         compAlg[8] = "=";
         compAlg[9] = "+";
         compAlg[10] = "-";
@@ -62,7 +62,7 @@ public class TablaTokens {
         Vector lineas[]=null;
         File f;
         FileReader fr;
-        boolean banErrores=false, banImprimir=false, banLeer=false, banTipo =false, banId=false, banLiteral=false;
+        boolean banErrores=false, banImprimir=false, banLeer=false, banTipo =false, banId=false, banSi=false , banSiNo=true;
 
         int cont=0;
         try {
@@ -104,11 +104,17 @@ public class TablaTokens {
                     palabra+=c;
                 }
                 else{
+                    // System.out.println(palabra);
                     posicion = j+1;
+                    banTipo=false;
+                    banSi=false;
+                    banSiNo=false;
                     if (palabra.compareTo(reservadas[0])==0) {
                         banImprimir =true;
                         banLeer =false;
-                        banLiteral =false;
+                        banTipo = false;
+                        banSi =false;
+                        banSiNo=false;
                         v = new Vector<>();
                         v.add(palabra);
                         v.add("");
@@ -118,7 +124,9 @@ public class TablaTokens {
                     else if(palabra.compareTo(reservadas[1])==0){
                         banImprimir =false;
                         banLeer =true;
-                        banLiteral =false;
+                        banTipo = false;
+                        banSi =false;
+                        banSiNo=false;
                         v = new Vector<>();
                         v.add(palabra);
                         v.add("");
@@ -128,27 +136,45 @@ public class TablaTokens {
                     else if(palabra.compareTo(reservadas[2])==0){
                         banImprimir =false;
                         banLeer =false;
-                        banLiteral =false;
+                        banTipo = false;
+                        banSi =true;
+                        banSiNo=false;
                     }
                     else if(palabra.compareTo(reservadas[3])==0){
                         banImprimir =false;
                         banLeer =false;
-                        banLiteral =false;
+                        banTipo = false;
+                        banSi =false;
+                        banSiNo=true;
                     }
                     else if(palabra.compareTo(reservadas[4])==0){
                         banImprimir =false;
                         banLeer =false;
-                        banLiteral =false;
+                        banTipo = false;
+                        banSi =false;
+                        banSiNo=false;
                     }
                     else if(palabra.compareTo(reservadas[5])==0){
                         banImprimir =false;
                         banLeer =false;
-                        banLiteral =false;
+                        banTipo = false;
+                        banSi =false;
+                        banSiNo=false;
                     }
                     else if(palabra.compareTo(reservadas[6])==0){
                         banImprimir =false;
                         banLeer =false;
-                        banLiteral =false;
+                        banTipo = false;
+                        banSi =false;
+                        banSiNo=false;
+                    }
+                    else if (palabra.compareTo(tipos[0])==0 || palabra.compareTo(tipos[1])==0 ||
+                    palabra.compareTo(tipos[2])==0 ||palabra.compareTo(tipos[3])==0 ||palabra.compareTo(tipos[4])==0){
+                        banImprimir =false;
+                        banLeer =false;
+                        banTipo = true;
+                        banSi =false;
+                        banSiNo=false;
                     }
                     break;
                 }
@@ -156,29 +182,29 @@ public class TablaTokens {
             }
             //imprimir
             if (banImprimir==true) {
-               palabra="";
-               if (auxL.charAt(posicion)==carSim[11] && auxL.charAt(auxL.length()-1)==';'){
-                  System.out.println(auxL.charAt(posicion));
-                  for (int j = posicion; j <auxL.length(); j++) {
-                     char c = auxL.charAt(j);
-                     if (c!= ';'){
+                palabra="";
+                if (auxL.charAt(posicion)==carSim[11] && auxL.charAt(auxL.length()-1)==';'){
+                    // System.out.println(auxL.charAt(posicion));
+                    for (int j = posicion; j <auxL.length(); j++) {
+                        char c = auxL.charAt(j);
+                        if (c!= ';'){
                            palabra+=c;
-                     }
-                     else {
-                        break;
-                     }
-                  }
-                  if (banErrores == false) {
-                     v = new Vector<>();
-                     v.add(palabra);
-                     v.add("literal");
-                     v.add(" ");
-                     tokens.add(v);
-                  }
-                  else{
-                     System.exit(0);
-                  }
-               }
+                        }
+                        else {
+                            break;
+                        }
+                    }
+                    if (banErrores == false) {
+                        v = new Vector<>();
+                        v.add(palabra);
+                        v.add("literal");
+                        v.add(" ");
+                        tokens.add(v);
+                    }
+                    else{
+                        System.exit(0);
+                    }
+                }
             }
             //leer
             if (banLeer == true) {
@@ -201,7 +227,6 @@ public class TablaTokens {
                             v.add("tipo");
                             v.add(" ");
                             tokens.add(v);
-                            banTipo=true;
                         }
                     }
                     palabra="";
@@ -234,13 +259,199 @@ public class TablaTokens {
                         }
                     }
 
-
                 }
+            }
+            if (banTipo ==true) {
+                v = new Vector<>();
+                v.add(palabra);
+                v.add("tipo");
+                v.add(" ");
+                tokens.add(v);
+                palabra="";
+                for (int j = posicion; j < auxL.length(); j++) {
+                    char c = auxL.charAt(j);
+                    if (c == carSim[8]) {
+                        palabra+=c;
+                    }
+                    else if(c != ' '&& c!= ';') {
+                        palabra+= c;
+                    }
+                    else{
+                        break;
+                    }
+                    if (palabra.charAt(0) != '!'&& palabra.length() >0) {
+                        banId=false;
+                    }
+                    else{
+                        banId = true;
+                    }
+                }
+                for (int j = 0; j <tipos.length; j++) {
+                    if (palabra.equals(tipos[j])==false && banId !=false) {
+                        v = new Vector<>();
+                        v.add(palabra);
+                        v.add("id");
+                        v.add(" ");
+                        tokens.add(v);
+                        break;
+                    }
+                }
+            }
+            if (banSi==true) {
+                v = new Vector<>();
+                v.add(palabra);
+                v.add("if");
+                v.add(" ");
+                tokens.add(v);
+                palabra="";
+                for (int j = posicion; j < auxL.length(); j++) {
+                    char c = auxL.charAt(j);
+                    if (c == carSim[3]) {
+                        palabra+=c;
+                    }
+                    else{
+                        posicion = j;
+                        break;
+                    }
+                }
+                v = new Vector<>();
+                v.add(palabra);
+                v.add("( apertura");
+                v.add(" ");
+                tokens.add(v);
+                palabra="";
+                
+                if (auxL.charAt(posicion) == carSim[8]) {
+                    for (int j = posicion; j < auxL.length(); j++) {
+                        char c = auxL.charAt(j);
+                        if (c == carSim[8] || c!= ' ') {
+                            palabra+=c;
+                        }
+                        else{
+                            posicion = j+1;
+                            break;
+                        }
+                    }
+                    v = new Vector<>();
+                    v.add(palabra);
+                    v.add("id");
+                    v.add(" ");
+                    tokens.add(v);
+                    palabra="";
+                }
+                else if(validarNum(auxL.charAt(posicion)) ==true){
+                    for (int j = posicion; j < auxL.length(); j++) {
+                        char c = auxL.charAt(j);
+                        if (c!= ' ') {
+                            palabra+=c;
+                        }
+                        else{
+                            posicion = j+1;
+                            break;
+                        }
+                    }
+                    v = new Vector<>();
+                    v.add(palabra);
+                    v.add("constante");
+                    v.add(" ");
+                    tokens.add(v);
+                    palabra="";
+                }
+
+                for (int j = posicion; j < auxL.length(); j++) {
+                    char c = auxL.charAt(j);
+                    if (c!= ' ') {
+                        palabra+=c;
+                    }
+                    else{
+                        posicion = j+1;
+                        break;
+                    }
+                }
+                for (int j = 0; j <6; j++) {
+                    if (palabra.compareTo(compAlg[j])==0) {
+                        v = new Vector<>();
+                        v.add(palabra);
+                        v.add("operador");
+                        v.add(" ");
+                        tokens.add(v);
+                        palabra="";
+                        break;
+                    }    
+                }
+
+                if (auxL.charAt(posicion) == carSim[8]) {
+                    for (int j = posicion; j < auxL.length(); j++) {
+                        char c = auxL.charAt(j);
+                        if (c == carSim[8] || c!= ' ' && c!= carSim[4]) {
+                            palabra+=c;
+                        }
+                        else{
+                            posicion = j+1;
+                            break;
+                        }
+                    }
+                    v = new Vector<>();
+                    v.add(palabra);
+                    v.add("id");
+                    v.add(" ");
+                    tokens.add(v);
+                    palabra="";
+                }
+                else if(validarNum(auxL.charAt(posicion)) ==true){
+                    for (int j = posicion; j < auxL.length(); j++) {
+                        char c = auxL.charAt(j);
+                        if (c!= ' ' && c!= carSim[4]) {
+                            palabra+=c;
+                        }
+                        else{
+                            posicion = j+1;
+                            break;
+                        }
+                    }
+                    v = new Vector<>();
+                    v.add(palabra);
+                    v.add("constante");
+                    v.add(" ");
+                    tokens.add(v);
+                    palabra="";
+                }
+                if (auxL.charAt(posicion)!= carSim[4]) {
+                    for (int j = posicion; j < auxL.length(); j++) {
+                        char c = auxL.charAt(j);
+                        if (c!= ' ') {
+                            palabra+=c;
+                        }
+                        else{
+                            posicion = j+1;
+                            break;
+                        }
+                    }
+                    if (palabra.compareTo(compAlg[6]) == 0 ||palabra.compareTo(compAlg[7]) == 0) {
+                        
+                    }
+                }
+            }
+            if (banSiNo==true) {
+                v = new Vector<>();
+                v.add(palabra);
+                v.add("else");
+                v.add(" ");
+                tokens.add(v);
+                palabra="";
             }
         }
 
         for (int i = 0; i < tokens.size(); i++) {
             System.out.println(tokens.get(i));
         }
+    }
+
+    public static boolean validarNum(char c) {
+        boolean b=false;
+        if (c >=48 && c<= 57) {
+            b=true;
+        }
+        return b;
     }
 }
